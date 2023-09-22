a883c38ed11bcf350dde34f3cf7f183d51b1cf65
JoonGit <ljj9730@gmail.com>
2023-09-22 14:33:54 +0900

    230922_ljj_04

---
+++ src/test/java/com/example/resource_project/ResourceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resource_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
